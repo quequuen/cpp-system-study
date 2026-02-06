5a3853390378c075238b279900fead252e06794e
quequuen <rkdusl2020@gmail.com>
2026-02-06 19:26:57 +0900

    Add: basics/01_variables/CONSTANTS.h 내 임시 상수 정리 및 main_01_variables_06.cpp 내 해당 상수들 사용

---
+++ basics/01_variables/CONSTANTS.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace CONSTANTS
+{
+    constexpr double PI = 3.14159265358979323846;
+    constexpr double EULER = 2.71828182845904523536;
+    constexpr double GOLDEN_RATIO = 1.618033988749895;
+} // namespace CONSTANTS
--- basics/01_variables/main_01_variables_06.cpp
+++ basics/01_variables/main_01_variables_06.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "CONSTANTS.h"
+#define TAX_RATE 0.1f
+// c++에서는 상수를 대체하기 위해서 매크로를 쓰는거는 안 쓴다고 보면 됨.
+// 대신에 const 키워드를 사용해서 상수를 정의함.
+// #define은 단순히 텍스트 치환이기 때문에 디버깅이 어렵고, 타입 검사가 안 됨.
+// const float tax_rate = 0.1f;
+using namespace std;
+using namespace CONSTANTS;
 
+int main()
+{
+    double radius;
+
+    int number = 123;
+    const float tax_rate = 0.1f;
+    
+    // int price = number * TAX_RATE;
+    int price = number * tax_rate;
+
+    cin >> radius;
+
+    double circumference = 2 * PI * radius;
+    double area = PI * radius * radius;
+
+
+    return 0;
+}
