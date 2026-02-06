be5796c73ded1bf7dfb5ac733b179d2ce527d817
quequuen <rkdusl2020@gmail.com>
2026-02-06 20:29:17 +0900

    Add, Docs: basics/02_control/main_02_control_02.cpp 내 증감 연산자 코드 일부 추가 및 notes/control.md 내 산술 연산자 표 정리

---
+++ basics/02_control/main_02_control_02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int one = 1;
+
+    cout << one << endl;
+    cout << one-- + 5 << endl;
+    cout << one << endl; 
+
+    cout << -7 % 3 << endl;
+    
+    return 0;
+}
