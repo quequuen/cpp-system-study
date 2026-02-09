cafc8db70ffc87c16a78a328332bf11775d37751
quequuen <rkdusl2020@gmail.com>
2026-02-10 02:14:17 +0900

    Add, Docs: notes/control.md 내 비트 연산자 내용 정리 추가, basics/02_control/main_02_control_03.cpp 추가 및 비트 연산자 관련 내용 추가

--- basics/02_control/main_02_control_03.cpp
+++ basics/02_control/main_02_control_03.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <bitset>
 
 int main()
 {
-    
+    using namespace std;
+
+    unsigned char a = 5;  // 00000101
+
+    cout << (int)a << endl;
+    cout << bitset<4>(a) << endl;
+
+    unsigned char b = a << 3; // 00101000
+
+    cout << (int)b << endl;
+    cout << bitset<8>(b) << endl;
+    cout << bitset<8>(b << 3) << " " << (b << 3) << endl;
+    cout << bitset<8>(~b) << " " << (~b) << endl;
+    cout << bitset<8>(5 | 12) << " " << (5 | 12) << endl;
+
     return 0; 
 }
