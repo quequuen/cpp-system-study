082adb79f6a512eb7a6879acd71bb15607a0df53
quequuen <rkdusl2020@gmail.com>
2026-02-15 01:51:17 +0900

    Feat: basics/06_arrays/main_06_arrays_03.cpp 내 내림차순 선택 정렬 코드 추가

--- basics/06_arrays/main_06_arrays_03.cpp
+++ basics/06_arrays/main_06_arrays_03.cpp
@@ -2,5 +2,24 @@
 
 using namespace std;
 
+    cout << "내림차순 선택 정렬" << endl;
+    for(int startIndex = 0; startIndex < length - 1; ++startIndex)
+    {
+        int biggestIndex = startIndex;
+
+        for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
+        {
+            if(array[biggestIndex] < array[currentIndex])
+            {
+                biggestIndex = currentIndex;
+            }
+        }
+
+        int temp = array[biggestIndex];
+        array[biggestIndex] = array[startIndex];
+        array[startIndex] = temp;
+
+        printArray(array, length);
+    }
     return 0;
 }
