b591d63101141d04e2cc15fc247f8206f07cff7b
quequuen <rkdusl2020@gmail.com>
2026-02-21 04:19:44 +0900

    Add, Docs: notes/arrays.md 내 일반 배열과 벡터의 차이 추가, bascis/06_arrays/main_06_arrays_06.cpp 내 std::vector 관련 코드 추가

--- basics/06_arrays/main_06_arrays_06.cpp
+++ basics/06_arrays/main_06_arrays_06.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int main() 
 {
-    std::vector<int> arr;
+    vector<int> v = {1, 2};
+    int* ptr = &v[0]; // v[0]의 주소를 기억해둠
+    
+    cout << &v << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+    cout << &v[0] << endl;
+    
+    v.push_back(3); // 만약 여기서 용량이 꽉 차서 이사를 갔다면?
+    // 이제 ptr이 가리키는 주소는 이미 삭제된 '옛날 집' 주소
+
+    cout << &v << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+    cout << &v[0] << endl;
+
+
     
     return 0;
 }
