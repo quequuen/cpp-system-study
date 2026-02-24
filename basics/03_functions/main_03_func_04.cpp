3766756d4680608eb9803f24ef1769d464fa7b96
quequuen <rkdusl2020@gmail.com>
2026-02-25 02:12:04 +0900

    Add, Docs: notes/functions.md 내 재귀 함수 디버깅 요령 추가, basics/03_functions/main_03_func_04.cpp 내 피보나치 재귀 함수 구현 코드 추가

--- basics/03_functions/main_03_func_04.cpp
+++ basics/03_functions/main_03_func_04.cpp
@@ -7,8 +7,12 @@ void countUp(int count)
 {
     cout << count << endl;
     if (count < 10) countUp(count + 1);
-    
+}
 
+int fib(int n) {
+    cout << "fib(" << n << ") 호출됨" << endl;
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
 }
 
 int main()
@@ -16,7 +20,13 @@ int main()
     std::vector<int> v{1, 2, 3};
     // int *v_ptr = new int[3]{1, 2, 3};
     
-    countUp(1);
+    // countUp(1);
+
+    cout << fib(6) << endl;
+
+    // 0 1 2 3 5 8 13;
+
+
 
     return 0;
 }
