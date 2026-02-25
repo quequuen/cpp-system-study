c2af198a386de8b8381ff4029d020ff73a8d6c7e
quequuen <rkdusl2020@gmail.com>
2026-02-25 17:07:53 +0900

    Add, Docs: notes/functions.md 내 가변 인자 함수 내용 정리 추가, basics/03_functions/main_03_func_06.cpp 내 가변 인자 함수 예시 코드 추가

--- basics/03_functions/main_03_func_06.cpp
+++ basics/03_functions/main_03_func_06.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cstdarg>
 
 using namespace std;
+
+double sum(int count, ...) {
+    double total = 0;
+    va_list args;      // 인자 목록을 저장할 변수
+    va_start(args, count); // 시작 지점 설정
+
+    for (int i = 0; i < count; ++i) {
+        total += va_arg(args, double); // 하나씩 꺼내기 (타입 명시 필요)
+    }
+
+    va_end(args);      // 정리
+    return total;
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,8 +25,8 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; ++i)
     {
         string argv_value = argv[i];
-
-        if(i == 1) 
+        std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+        if(i == 1) // 인수 타입 변환 및 출력
         {
             int input_value = std::stoi(argv_value);
             cout << input_value + 1 << endl;
@@ -20,5 +34,7 @@ int main(int argc, char *argv[])
         }
         else cout << argv_value << endl;
     }
+
+
     return 0;
 }
