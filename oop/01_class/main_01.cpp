357437c4a56a4c9e2ee261c5afbd737cd09d08b2
quequuen <rkdusl2020@gmail.com>
2026-02-27 14:19:43 +0900

    Add, Docs: notes/oop.md 내 생성자 내용 정리, oop/01_class/main_01.cpp 내 생성자 추가

--- oop/01_class/main_01.cpp
+++ oop/01_class/main_01.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <format>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -12,6 +15,34 @@ class Date
     int _year;
 
     public:
+
+    // 생성자
+    Date()
+    {
+        // 현재 시간 가져옴
+        auto now = chrono::system_clock::now();
+        std::time_t now_time = chrono::system_clock::to_time_t(now);
+
+        // 로컬 시간으로 변환
+        tm* local_time = localtime(&now_time);
+
+        int year = local_time->tm_year + 1900;
+        int month = local_time->tm_mon + 1; 
+        int day = local_time->tm_mday;
+
+        _month = month;
+        _day = day;
+        _year = year;
+
+    }
+
+    Date(const int &m, const int &d, const int &y)
+    {
+        _month = m;
+        _day = d;
+        _year = y;
+    }
+
     void setDate(const int &m, const int &d, const int &y)
     {
        _month = m;
@@ -34,22 +65,24 @@ class Date
         _year = y;
     }
 
-    string getDate()
+    string getDate() const 
     {
         return to_string(_month) + "." + to_string(_day) + "." + to_string(_year);
+        // C++ 20 이상 가능
+        // return std::format("{:02}.{:02}.{}", _month, _day, _year);
     }
 
-    int getMonth()
+    int getMonth() const 
     {
         return _month;
     }
 
-    int getDay()
+    int getDay() const
     {
         return _day;
     }
 
-    int getYear()
+    int getYear() const 
     {
         return _year;
     }
@@ -76,6 +109,10 @@ int main()
 
     cout << today.getDate() << endl;
     cout << today_c.getDate() << endl;
+
+    Date today2;
+
+    cout << today.getDate() << endl;
 
     return 0;
 }
