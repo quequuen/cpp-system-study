54b4b64a16278933f90a4d91f008a10156526104
quequuen <rkdusl2020@gmail.com>
2026-05-14 14:27:58 +0900

    study: 기본적인 파일 입출력 리팩토링

--- basics/10_file/file_01.cpp
+++ basics/10_file/file_01.cpp
@@ -8,15 +8,19 @@ int main() {
   // writing
   // ofstream: output file stream
   // std::ios의 자식 클래스임.
-  std::ofstream ofs("hello.dat");  // ios.app, ios::binary
-  // ofs.open("hello.dat");
-  // 생성자를 이용해 열 수도, open() 메소드를 이용해 열 수도 있다.
-  // build 아래에 해당 이름의 파일 생성됨.
+  std::ofstream ofs("hello.dat");
+  // std::ios::app(append 모드), std::ios::binary(binary 저장), std::ios::ate
+  // (파일을 열자마자 포인터를 끝으로 보냄), std::ios::trunc(파일을 열 때 기존
+  // 내용을 모두 지움. 기본값임) ofs.open("hello.dat"); 생성자를 이용해 열 수도,
+  // open() 메소드를 이용해 열 수도 있다. build 아래에 해당 이름의 파일 생성됨.
 
-  // 파일이 존재 하지 않는다면 출력 하고 종료
-  if (!ofs) {
+  // 파일이 없거나 권한 문제, 경로 문제 등으로 안 열렸을 때 체크
+  if (!ofs.is_open()) {
     std::cerr << "Couldn't open file!" << std::endl;
-    exit(1);
+    // exit(1);
+    return 1;
+    // C++ main 함수에서는 이 방법이 좀 더 권장됨.
+    // 객체들의 소멸자가 더 안전하게 호출됨.
   }
 
   ofs << "안녕 내 이름은" << std::endl;
@@ -28,15 +32,20 @@ int main() {
   std::ifstream ifs("hello.dat");
 
   // read
-  if (!ifs) {
+  if (!ifs.is_open()) {
     std::cerr << "Couldn't open file!" << std::endl;
-    exit(1);
+    return 1;
   }
+  std::string str;
 
-  while (ifs) {
-    std::string str;
-    getline(ifs, str);
+  //   while (ifs) {
+  //     getline(ifs, str);
 
+  //     std::cout << str << std::endl;
+  //   }
+  // 마지막 줄을 읽고 난 뒤 한 번 더 실행되어 빈 줄이 출력됨.
+
+  while (getline(ifs, str)) {
     std::cout << str << std::endl;
   }
   return 0;
